2e26cdbe47c3dd92f16dd9ec5ec8426ddf23e871
Kevin Jayne <kevin.jayne@icloud.com>
2018-06-08 01:25:34 -0400

    helloooooooo world

--- src/main/kotlin/com/wework/demo/DemoApplication.kt
+++ src/main/kotlin/com/wework/demo/DemoApplication.kt
@@ -2,10 +2,20 @@ package com.wework.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
+}
+
+@RestController
+class GreetingController {
+
+ @GetMapping("/hello/{name}")
+ fun get(@PathVariable name: String) = "Hello, $name"
 }
